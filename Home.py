c63ebc5c539b35a05429ffd97e10ef1ad3618476
Aditya <apocalypse20101@gmail.com>
2023-06-25 11:57:12 +0530

    Update data.csv

--- Home.py
+++ Home.py
@@ -25,7 +25,7 @@ content2 = """
            Below you can find some of the apps I have built in Python. Feel free to contact me!
            """
 
-st.subheader(content2)
+st.info(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
