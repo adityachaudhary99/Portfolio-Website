4cf308b452a27e6f279407b71b849f99c3fd1d35
Aditya <apocalypse20101@gmail.com>
2023-07-07 13:55:54 +0530

    Updates

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.title("Home")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpeg")
 
 with col2:
     st.title("Aditya Chaudhary")
@@ -32,15 +32,21 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=';')
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:5].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image('images/' + row['image'])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[5:10].iterrows():
         st.header(row['title'])
         st.write(row['description'])
         st.image('images/' + row['image'])
         st.write(f"[Source Code]({row['url']})")
+
+for index, row in df[10:].iterrows():
+    st.markdown(f"<h1 style='text-align: center;'> {row['title']} </h1>", unsafe_allow_html=True)
+    st.markdown(f"<p style='text-align: center;'> {row['description']}</p>", unsafe_allow_html=True)
+    st.image('images/' + row['image'])
+    st.write(f"[Source Code]({row['url']})")
